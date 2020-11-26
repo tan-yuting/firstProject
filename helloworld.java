52e2d0f2c963667e67b694edba50aa7856bdc1b8
tan-yuting <1986757473@qq.com>
2020-11-26 16:35:41 +0800

    hello,nihaowa

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+public class helloworld {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello,my love");
+        System.out.println("hello,beijing");
+        System.out.println("hello,Squera");
+    }
+}
